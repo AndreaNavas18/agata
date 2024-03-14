1a08dde66c8cfce083948c902912a021404a8ec6
KarenC23 <klcaicedo93@gmail.com>
2024-03-14 17:25:52 -0500

    COMMITNEW

--- resources/views/emails/new_ticket.blade.php
+++ resources/views/emails/new_ticket.blade.php
@@ -1,24 +1,16 @@
 @extends('layouts.email')
 
 
-@section('message')
-    <p style="font-size: 20px;margin-bottom: 40px; color: black;">
-        Le informamos que hemos registrado su solicitud como un nuevo ticket de soporte. Estamos trabajando para atender su
-        requerimiento y nos comunicaremos con usted pronto. A continuación, encontrará los detalles correspondientes.
-    </p>
-@endsection
-
 
 @section('content')
     <div
         style="
-    font-size: medium;
-    padding-left: 50px;
-    padding-right: 50px;
-    padding-top: 165px;
-    width: 65%;
-    height: 100%;
-    color: black;
+            font-size: medium;
+            padding-left: 50px;
+            padding-right: 50px;
+            color: black;
+            position: absolute;
+            top: 365px;
 ">
 
         <div>
@@ -30,6 +22,7 @@
             <span><b>Tipo servicio:</b></span>
             {{ isset($ticket->service->service->name) ? $ticket->service->service->name : 'N/A' }}
         </div>
+
         <div>
             <span><b>Descripción servicio:</b></span>
             {{ isset($ticket->service->description) ? $ticket->service->description : 'N/A' }}
@@ -37,36 +30,32 @@
 
         <div>
             <span><b>Asunto:</b></span>
-            {{ isset($ticket->ticket_issue) ? $ticket->ticket_issue : 'N/A' }}</td>
+            {{ isset($ticket->ticket_issue) ? $ticket->ticket_issue : 'N/A' }}
         </div>
 
         <div>
-            <span><b>Agente:</b></span>
-            {{ isset($ticket->employee->short_name) ? $ticket->employee->short_name : 'N/A' }}</td>
+            <span><b>AGENTECAMBIO:</b></span>
+            {{ isset($ticket->employee->short_name) ? $ticket->employee->short_name : 'N/A' }}
         </div>
 
-        <div style="margin: 0 auto;  padding-top: 20px; position: relative;
-            top: 144px; left: 34%;">
+        {{-- <div style="
+            padding-top: 80px;
+            margin-left: 30%;">
             <button
                 style="
-                text-align: left;
-                background-color: blue;
-                border-radius: 15px;
-                height: 30px;
-                color: white;
-                border: none; 
-                cursor: pointer; 
-                display: flex; 
-                align-items: center; 
-                justify-content: center; 
-                text-decoration: none; ">
+                text-align: center;
+                        background-color: #2196f3;
+                        border-radius: 15px;
+                        height: 30px;
+                        color: white;
+                        border: solid 1px white;
+                        width: 180px;">
                 <a href="https://www.stratecsa.com/"
                     style="
             color: inherit; /* Hereda el color del texto del botón */
             text-decoration: none; /* Quita el subrayado */
-        ">Para
-                    más información</a></button>
-
-        </div>
+        ">Para más información</a></button>
+        CAMBIO PARA STAGED
+        </div> --}}
     </div>
 @endsection
