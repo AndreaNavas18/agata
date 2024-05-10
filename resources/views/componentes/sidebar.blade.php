fc72b000b186b5eef42232376ebe64745b1994cb
AndreaNavas18 <karennavas333@gmail.com>
2024-05-10 16:32:15 -0500

    cambiosidebar

--- resources/views/componentes/sidebar.blade.php
+++ resources/views/componentes/sidebar.blade.php
@@ -31,6 +31,59 @@
             </ul>
         </li>
     @endif
+    @if(auth()->user()->can('commercial.show'))
+    <li class="slide">
+       <a class="side-menu__item"  data-toggle="slide" href="#">
+           <i class="side-menu__icon fas fa-clipboard-check"></i>
+           <span class="side-menu__label">
+               Comercial
+           </span>
+           <i class="angle fa fa-angle-right"></i>
+       </a>
+       <ul class="slide-menu">
+           {{-- @can('permissions.index') --}}
+           <li><a class="slide-item loading"  href="{{ route('tariff.index') }}"><span>Tarifario</span></a></li>
+           {{-- @endcan --}}
+       </ul>
+    </li>
+    @endif
+
+    @if(auth()->user()->can('tickets.index'))
+   
+    <li>
+        <a class="side-menu__item loading" href="{{ route('tickets.index') }}">
+            <i class="side-menu__icon fas fa-ticket-alt"></i>
+            <span class="side-menu__label">Tickets</span>
+        </a>
+    </li>
+   @endif
+
+   @if(auth()->user()->can('services.index'))
+        <li>
+            <a class="side-menu__item loading" href="{{ route('customers.services.index.all') }}">
+                <i class="side-menu__icon fas fa-list-alt"></i>
+                <span class="side-menu__label"> Servicios</span>
+            </a>
+        </li>
+    @endif
+
+    @if(auth()->user()->can('customers.index'))
+    <li>
+        <a class="side-menu__item loading" href="{{ route('customers.index') }}">
+            <i class="side-menu__icon fas fa-user-friends"></i>
+            <span class="side-menu__label"> Clientes </span>
+        </a>
+    </li>
+    @endif
+    
+    @if(auth()->user()->can('providers.index'))
+        <li>
+            <a class="side-menu__item loading" href="{{ route('providers.index') }}">
+                <i class="side-menu__icon fas fa-broadcast-tower"></i>
+                <span class="side-menu__label"> Proveedores</span>
+            </a>
+        </li>
+    @endif
 
     {{-- @if(auth()->user()->can('parametros.empleados.index') || auth()->user()->can('parametros.general.index')) --}}
     @if (in_array(Auth()->user()->role_id, [1, 2, 3, 6, 7, 8, 9, 11]))
@@ -52,6 +105,7 @@
             </ul>
         </li>
     @endif
+
     @if (in_array(Auth()->user()->role_id, [5, 10, 9]))
     {{-- @if(auth()->user()->can('parametros.general.soporte')) --}}
         <li class="slide">
@@ -67,29 +121,12 @@
             </ul>
         </li>
     @endif
-    @if(auth()->user()->can('customers.index'))
-        <li>
-            <a class="side-menu__item loading" href="{{ route('customers.index') }}">
-                <i class="side-menu__icon fas fa-user-friends"></i>
-                <span class="side-menu__label"> Clientes </span>
-            </a>
-        </li>
-    @endif
     
     @if(auth()->user()->can('employees.index'))    
         <li>
             <a class="side-menu__item loading" href="{{ route('employees.index') }}">
                 <i class="side-menu__icon  fas fa-user-tie"></i>
                 <span class="side-menu__label"> Empleados</span>
-            </a>
-        </li>
-    @endif
-
-    @if(auth()->user()->can('providers.index'))
-        <li>
-            <a class="side-menu__item loading" href="{{ route('providers.index') }}">
-                <i class="side-menu__icon fas fa-broadcast-tower"></i>
-                <span class="side-menu__label"> Proveedores</span>
             </a>
         </li>
     @endif
@@ -101,41 +138,6 @@
                 <span class="side-menu__label"> Proyectos</span>
             </a>
         </li>
-    @endif
-
-    @if(auth()->user()->can('services.index'))
-        <li>
-            <a class="side-menu__item loading" href="{{ route('customers.services.index.all') }}">
-                <i class="side-menu__icon fas fa-list-alt"></i>
-                <span class="side-menu__label"> Servicios</span>
-            </a>
-        </li>
-    @endif
-    @if(auth()->user()->can('tickets.index'))
-   
-    <li>
-        <a class="side-menu__item loading" href="{{ route('tickets.index') }}">
-            <i class="side-menu__icon fas fa-ticket-alt"></i>
-            <span class="side-menu__label">Tickets</span>
-        </a>
-    </li>
-   @endif
-
-   @if(auth()->user()->can('commercial.show'))
-    <li class="slide">
-       <a class="side-menu__item"  data-toggle="slide" href="#">
-           <i class="side-menu__icon fas fa-clipboard-check"></i>
-           <span class="side-menu__label">
-               Comercial
-           </span>
-           <i class="angle fa fa-angle-right"></i>
-       </a>
-       <ul class="slide-menu">
-           {{-- @can('permissions.index') --}}
-           <li><a class="slide-item loading"  href="{{ route('tariff.index') }}"><span>Tarifario</span></a></li>
-           {{-- @endcan --}}
-       </ul>
-    </li>
     @endif
 </ul>
 
