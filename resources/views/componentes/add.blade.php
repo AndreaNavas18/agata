229362a8b286b9ce0c1f3d52742d61b06713cb15
KarenC23 <klcaicedo93@gmail.com>
2024-05-14 17:59:19 -0500

    Avances en el Modulo de Comercial: Crear Tarifas.

---
+++ resources/views/componentes/add.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <button class="btn btn-success
+        btn-sm"
+        type="submit" 
+        onclick="agregarPersona()">
+        <i class="fas fa-add"></i>
+        Agregar
+    </button>
