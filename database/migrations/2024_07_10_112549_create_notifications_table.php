ae7fff917e16b81b1d97f38ae1b6f30fb24b2b01
AndreaNavas18 <karennavas333@gmail.com>
2024-07-11 16:13:41 -0500

    cambiomirggracion

--- database/migrations/2024_07_10_112549_create_notifications_table.php
+++ database/migrations/2024_07_10_112549_create_notifications_table.php
@@ -16,13 +16,10 @@ return new class extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('type');
-            $table->morphs('notifiable',131);
-            $table->uuid('notifiable_id');
+            $table->morphs('notifiable');
             $table->text('data');
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
-
-            $table->index(['notifiable_type', 'notifiable_id']);
         });
     }
 
